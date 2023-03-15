af3809254822aae06eabe8ff9718719d89d9101f
akihito <kohiga@gmail.com>
2023-03-15 14:35:53 +0900

    modified makened script

--- README.md
+++ README.md
@@ -9,31 +9,31 @@
  -- More information about Manhattan Mobility Model
     https://sumo.dlr.de/docs/Tutorials/Manhattan.html
 
-1. Generate Vehicle's equipment file
+2. Generate Vehicle's equipment file
   (-s [wifi|camera|lidar] [all|random] -n <number of vehicles>)
     > python .\sensormount.py -s wifi all -s camera random -n 100 
 
-1. Generate deployment plan for each vehicle
+3. Generate deployment plan for each vehicle
     > python deployment.py -x 900.0 -y 900.0 -n 9
   (-x <vertial size of a map> -y <horizontal size of a map> -n <a number of mahines to deploy VMs>)
 
-1. Run Interaction Search
+4. Run Interaction Search
     > python .\interactionsearch.py -f .\sumotrace.xml -e .\vehicles.out -s sensors.json -t 0.0
   (-f <sumo trace file> -e <vehicle's equipment file> -s <sensor spec file> -t <a target vehicle ID>)
   (<sensor spec file> has already prepared. Do not modify)
 
-1. Copy the files in results and locale directory to inputs directory at Omnet++ samples
+5. Copy the files in results and locale directory to inputs directory at Omnet++ samples
     > mv .\results ..\omnetpp-6.0\samples\interaction-search\inputs
     > mv .\locale ..\omnetpp-6.0\samples\interaction-search\inputs
     (The location of inputs directory depends on the place you put omnet++ files)
 
-2. Run Omnet++ command line from mingw shortcut in omnet directory
+6. Run Omnet++ command line from mingw shortcut in omnet directory
     > C:\Users\kohig\Works\omnetpp-6.0\mingwenv.cmd
 
-3. Run Omnet++ simulation
+7. Run Omnet++ simulation
     > cd \Users\kohig\Works\omnetpp-6.0\samples\interaction-search
     > opp_run -m -u Cmdenv -c General -n . -n ../queueinglib -l ../queueinglib/libqueueinglib.dll priorityControl.ini
 
-4. Acquire Result files
+8. Acquire Result files
     Omnet++ simulation(Sequence.6) generates result files that record several parameters of Sink Object in Omnet++ simulation.
     
--- makened.py
+++ makened.py
@@ -3,6 +3,9 @@ import argparse
 
 parser = argparse.ArgumentParser(description="make nedfile for omnet++")
 parser.add_argument('-d', '--directory', type=str, default="results", help='a directory name that contains priority files')
+parser.add_argument('-c', '--cpu', type=int, required=True, help='a number of CPUs in one PC')
+parser.add_argument('-n', '--node', type=int, required=True, help='a number of PC')
+parser.add_argument('-o', '--output', type=str, default="test.ned", help='an output filename')
 
 args = parser.parse_args()
 
@@ -21,57 +24,86 @@ network PriorityControl
     submodules:
 '''
 
-submodules = '''
-        classifier%d: Classifier {
-        }
-        hangingUp%d: Sink {
-        }
-        High_Priority_Queue%d: PassiveQueue {
-        }
-        Low_Priority_Queue%d: PassiveQueue {
-        }
-        CPU%d: Server {
-        }
-        CPU%d: Server {
-        }
-        Served%d: Sink {
-        }
-        Served%d: Sink {
-        }'''
-
-connection = '''
-    connections:
-        classifier%d.out++ --> High_Priority_Queue%d.in++;
-        classifier%d.out++ --> Low_Priority_Queue%d.in++;
-        classifier%d.rest --> hangingUp%d.in++;
-        High_Priority_Queue%d.out++ --> CPU%d.in++;
-        High_Priority_Queue%d.out++ --> CPU%d.in++;
-        Low_Priority_Queue%d.out++ --> CPU%d.in++;
-        Low_Priority_Queue%d.out++ --> CPU%d.in++;
-        CPU%d.out --> Served%d.in++;
-        CPU%d.out --> Served%d.in++;'''
-
 tail = '''
 }
 '''
 
-vehicles = [f for f in os.listdir(args.directory) if os.path.isfile(os.path.join(args.directory, f))]
-vehicles_submodules = ""
-for v in vehicles:
-    vehicles_submodules = vehicles_submodules + '''
-        %s: FileBasedSource{
-        }''' % v
+def node_connection(node):
+    _cls = node['cls']
+    _hng = node['hng']
+    _hq  = node['hq']
+    _lq  = node['lq']
+    _cpus = node['cpu']
+    _srvs = node['srv']
 
-vehicles_connections = ""
-for v in vehicles:
-    vehicles_connections = vehicles_connections + '''
-        %s.out --> classifier%d.in++;''' % (v, 1)
+    ret = []
+    ret.append(_cls + ".out++ --> " + _hq + ".in++;")
+    ret.append(_cls + ".out++ --> " + _lq + ".in++;")
+    ret.append(_cls + ".rest --> " + _hng + ".in++;")
+    for i in range(len(_cpus)):
+        ret.append(_hq + ".out++ --> " + _cpus[i] + ".in++;")
+        ret.append(_lq + ".out++ --> " + _cpus[i] + ".in++;")
+        ret.append(_cpus[i] + ".out --> " + _srvs[i] + ".in++;")
+    ret.append("router.out ++ --> " + _cls + ".in++;")
 
+    return ret
 
-with open("test.ned", "w") as f:
-    f.write(header)
-    f.write(submodules % (1,1,1,1,1,2,1,2))
-    f.write(vehicles_submodules)
-    f.write(connection % (1,1,1,1,1,1,1,1,1,2,1,1,1,2,1,1,2,2))
-    f.write(vehicles_connections)
-    f.write(tail)
+if __name__ == '__main__':
+    ## make label list
+    vehicles = [f for f in os.listdir(args.directory) if os.path.isfile(os.path.join(args.directory, f))]
+    cpus  = ["CPU{:d}".format(i) for i in range(1, args.cpu * args.node + 1)]
+    highq = ["High_Priority_Queue{:d}".format(i) for i in range(1, args.node + 1)]
+    lowq  = ["Low_Priority_Queue{:d}".format(i) for i in range(1, args.node + 1)]
+    cls   = ["Classifier{:d}".format(i) for i in range(1, args.node + 1)]
+    hng   = ["hangingUp{:d}".format(i) for i in range(1, args.node + 1)]
+    srv   = ["Served{:d}".format(i) for i in range(1, args.cpu * args.node + 1)]
+
+    ## make submodule list
+    modules = []
+    modules += [x + ": FileBasedSource {\n\t\t\t}" for x in vehicles]
+    modules += [x + ": Server {\n\t\t\t}" for x in cpus]
+    modules += [x + ": PassiveQueue {\n\t\t\t}" for x in highq]
+    modules += [x + ": PassiveQueue {\n\t\t\t}" for x in lowq]
+    modules += [x + ": Sink {\n\t\t\t}" for x in srv]
+    modules += [x + ": Sink {\n\t\t\t}" for x in hng]
+    modules += [x + ": Classifier {\n\t\t\t}" for x in cls]
+    modules += ["router: Router {\n\t\t\t}"]
+
+    ## make node set
+    nodelist = []
+    for i in range(args.node):
+        node = {}
+        node['cpu'] = [cpus.pop(0) for i in range(args.cpu)]
+        node['hq'] = highq.pop(0)
+        node['lq'] = lowq.pop(0)
+        node['cls'] = cls.pop(0)
+        node['hng'] = hng.pop(0)
+        node['srv'] = [srv.pop(0) for i in range(args.cpu)]
+        nodelist.append(node)
+
+    ## make connection for each node
+    connections = sum([node_connection(node) for node in nodelist], [])
+
+    ## make connection between a router and vehicles
+    connections += [vehicle + ".out --> router.in++;"for vehicle in vehicles]
+
+    ## write out a ned file
+    with open(args.output, "w") as f:
+        f.write(header)
+        for i in modules:
+            f.write("\t\t\t" + i + "\n")
+        f.write("\t\tconnections:")
+        for i in connections:
+            f.write("\t\t\t" + i + "\n")
+        f.write(tail)
+
+# vehicles_submodules = ""
+# for v in vehicles:
+#     vehicles_submodules = vehicles_submodules + '''
+#         %s: FileBasedSource{
+#         }''' % v
+
+# vehicles_connections = ""
+# for v in vehicles:
+#     vehicles_connections = vehicles_connections + '''
+#         %s.out --> classifier%d.in++;''' % (v, 1)
